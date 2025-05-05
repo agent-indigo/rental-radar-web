8672dcbb6e8ee0f56d0f58f26122ebd814fb67bf
agent-indigo <bradenhynes@gmail.com>
2025-05-05 20:38:41 -0230

    Error 500 page improvements

--- app/error.tsx
+++ app/error.tsx
@@ -7,17 +7,29 @@ import ErrorPageProps from '@/types/ErrorPageProps'
 const ErrorPage: FunctionComponent<ErrorPageProps> = ({
   error,
   reset
-}): ReactElement => (
-  <section>
-    <h1>
-      500: Internal Server Error
-    </h1>
-    <p>
-      {error.toString()}
-    </p>
-    <button onClick={(): void => reset()}>
-      Retry
-    </button>
-  </section>
-)
+}): ReactElement => {
+  const {
+    message,
+    stack
+  }: Error = error
+  return (
+    <section>
+      <h1>
+        500: Internal Server Error
+      </h1>
+      <p>
+        {message}
+      </p>
+      <h2>
+        Stack
+      </h2>
+      <p>
+        {stack}
+      </p>
+      <button onClick={(): void => reset()}>
+        Retry
+      </button>
+    </section>
+  )
+}
 export default ErrorPage
--- httpResponses/error500response.ts
+++ httpResponses/error500response.ts
@@ -1,10 +1,16 @@
 import {NextResponse} from 'next/server'
-const error500response: Function = (error: Error): NextResponse => new NextResponse(
-  JSON.stringify({
-    message: error.message,
-    stack: process.env.NODE_ENV === 'development' ? error.stack : undefined
-  }), {
-    status: 500
-  }
-)
+const error500response: Function = (error: Error): NextResponse => {
+  const {
+    message,
+    stack
+  }: Error = error
+  return new NextResponse(
+    JSON.stringify({
+      message: message,
+      stack: process.env.NODE_ENV === 'development' ? stack : undefined
+    }), {
+      status: 500
+    }
+  )
+}
 export default error500response
